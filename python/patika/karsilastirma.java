fe1c837b8b79f4fec204aa25bb5a14885edf41da
Fatih <fatih03uyanik@gmail.com>
2022-03-15 01:23:02 +0300

    Delete karsilastirma.java

--- python/patika/karsilastirma.java
+++
@@ -1,8 +0,0 @@
-public class karsilastirma {
-    public static void main(String[] args) {
-        Integer not = 50;
-        System.out.println(not > 45 ? "Geçti" : "Kaldı");
-        System.out.println(not > 70 ? "Yüksek" : "Düşük");
-        System.out.println(++not);
-    }
-}
